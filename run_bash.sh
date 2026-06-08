d99abd93c3bf6e57846b6426d3838efa361dfa47
Allan Fel <allan.goioere@hotmail.com>
2026-06-08 12:06:20 -0300

    feat: Atualizar script run_bash.sh para incluir limpeza de cache e sincronização de dependências

--- run_bash.sh
+++ run_bash.sh
@@ -2,9 +2,15 @@
 
 echo "Syncing dependencies and cleaning cache..."
 
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
 $HOME/.local/bin/uv sync
 $HOME/.local/bin/uv clean cache
 $HOME/.local/bin/uv clean size
+
+git clean -fdx
+git gc --prune=now
+git rm --cached -r .
 
 echo "Running the bot..."
 
