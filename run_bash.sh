10c1e02879df59144f5c92428e14f846b632495c
Allan Fel <allan.goioere@hotmail.com>
2026-06-08 11:58:25 -0300

    feat: Melhorar script run_bash.sh com mensagens de status

--- run_bash.sh
+++ run_bash.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+echo "Syncing dependencies and cleaning cache..."
+
 $HOME/.local/bin/uv sync
+$HOME/.local/bin/uv clean cache
+$HOME/.local/bin/uv clean size
+
+echo "Running the bot..."
 
 $HOME/.local/bin/uv run main.py
